29c2159beb93146d3d8ce87b9b1798b05384e830
derikpimentel <derikpimentel@hotmail.com.br>
2024-11-06 15:05:10 -0300

    add entry

--- main.py
+++ main.py
@@ -35,6 +35,41 @@ class Application:
         #self.button["image"] = img
         self.button.pack()
 
+        # Criando um container para posicionar os widgets com grid()
+        self.container_entry = Frame(self.master)
+        self.container_entry.pack()
+
+        """ Campo de entrada de texto com linha única """
+        self.login_entry = Entry(self.container_entry) #  Cria um Entry
+        self.login_entry["width"] = 15
+        self.login_entry["bg"] = "lightgray"
+        self.login_entry["fg"] = "blue"
+        self.login_entry["font"] = ("Helvetica", 12, "italic")
+        self.login_entry.insert(0, "Usuário") # Define um texto inicial
+        self.login_entry["state"] = "normal" # ('disabled', 'normal' e 'readonly')
+        self.login_entry.grid(row=0, column=0) # Posiciona por linha e coluna na janela
+
+        self.key_entry = Entry(self.container_entry)
+        self.key_entry["show"] = "*" # Substitui o texto digitado por um caractere específico
+        self.key_entry["width"] = 15
+        self.key_entry["font"] = ("Helvetica", 12)
+        self.key_entry.insert(0, "Senha")
+        self.key_entry.grid(row=1, column=0)
+
+        self.key_button = Button(self.container_entry, text="Mostar Senha", command=self.key_show)
+        self.key_button.grid(row=1, column=1)
+
+        self.login_button = Button(self.container_entry, text="Entrar", command=self.authenticate, padx=40)
+        self.login_button.grid(row=2, columnspan=2)
+
+        self.status_con = StringVar() # Variável para texto dinâmico
+
+        self.status_entry = Entry(self.container_entry)
+        self.status_entry["textvariable"] = self.status_con
+        self.status_entry["width"] = 37
+        self.status_entry["state"] = "readonly" # Apenas leitura
+        self.status_entry.grid(row=3, columnspan=2)
+
     # Função a ser executada ao clicar no botão
     def on_click(self):
         print("Botão pressionado!")
@@ -42,6 +77,31 @@ class Application:
         self.button.config(text="Clicado!") # Altera o texto do Button
         self.button.config(bg="gray") # Altera a cor de fundo do Button
         self.button.config(state="disabled") # Altera o estado do Button
+
+    # Função para exibir e ocultar senha
+    def key_show(self):
+        if self.key_entry["show"] == "*":
+            self.key_entry.config(show="")
+            self.key_button.config(text="Ocultar Senha")
+        else:
+            self.key_entry.config(show="*")
+            self.key_button.config(text="Mostar Senha")
+
+    # Função para TESTES de autenticação
+    def authenticate(self):
+        login = self.login_entry.get()
+        key = self.key_entry.get()
+        print(f"Usuário: '{login}'")
+        if (login == "Derik") and (key == "12345"):
+            print("Conexão bem sucedida!")
+            self.status_con.set("Conectado!") # Atualiza a variável
+            self.login_entry.delete(0, "end") # Remove o texto do campo
+            self.login_entry.insert(0, "Usuário")
+            self.key_entry.delete(0, "end")
+            self.key_entry.insert(0, "Senha")
+        else:
+            print("Falha de conexão!")
+            self.status_con.set("Usuário e/ou senha incorretos!")
 
 # Estrutura de execução
 if __name__ == "__main__":
