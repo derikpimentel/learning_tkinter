b65f2ff49c68877f32a2d67b01be76be6d2e6b72
derikpimentel <derikpimentel@hotmail.com.br>
2024-11-13 10:04:22 -0300

    add panedwindow

--- submain.py
+++ submain.py
@@ -30,3 +30,29 @@ class Subapplication:
         Label(self.container, text="Idade").grid(row=1, column=0, sticky="w")
         Entry(self.container).grid(row=1, column=1)
         Button(self.container, text="Salvar").grid(row=2, column=0, columnspan=2)
+
+        # Cria um PanedWindow (Janela de painéis)
+        self.h_paned = PanedWindow(self.master)
+        self.h_paned["orient"] = "horizontal"
+        self.h_paned.pack(fill="both", expand=True)
+
+        self.v_paned = PanedWindow(self.h_paned, orient="vertical")
+        self.h_paned.add(self.v_paned, minsize=100) # Acrescenta o painel com tamanho mínimo
+
+        self.left_label = Label(self.v_paned)
+        self.left_label["text"] = "Painel esquerdo superior"
+        self.left_label["bg"] = "lightyellow"
+        self.v_paned.add(self.left_label, minsize=100)
+
+        self.v_paned.add(Label(self.v_paned, text="Painel esquerdo inferior"), minsize=100)
+
+        self.center_label = Label(self.h_paned)
+        self.center_label["text"] = "Painel central"
+        self.center_label["bg"] = "lightblue"
+        self.h_paned.add(self.center_label, minsize=100)
+        self.h_paned.forget(self.center_label) # Remove o painel
+
+        self.right_label = Label(self.h_paned)
+        self.right_label["text"] = "Painel direito"
+        self.right_label["bg"] = "lightgreen"
+        self.h_paned.add(self.right_label, minsize=100)
